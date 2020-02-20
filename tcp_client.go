9a3b7e7921a99aa45f4c61e050ec61e8b8f28b74
Tobi <inf17127@lehre.dhwb-stuttgart.de>
2020-02-20 14:13:46 +0100

    Rewrite tcp_client to use tls

--- tcp_client.go
+++ tcp_client.go
@@ -1,45 +1,36 @@
 package Studienarbeit_src
 
 import (
-	"bufio"
-	"fmt"
-	"net"
-	"os"
-	"strings"
+	"crypto/tls"
+	"log"
 )
 
-func Tcp_client() {
-	// connect to this socket
-	conn, _ := net.Dial("tcp", "127.0.0.1:8081")
-	for {
-		// read in input from stdin
-		reader := bufio.NewReader(os.Stdin)
-		fmt.Print("Text to send: ")
-		text, _ := reader.ReadString('\n')
-		// send to socket
-		fmt.Fprintf(conn, text+"\n")
-		// listen for reply
-		message, err := bufio.NewReader(conn).ReadString('\n')
-		//trimm String without \r\n
-		if err != nil {
-			fmt.Println(err.Error())
-		}
-		for {
-			//check if the transmittet message is correctly received
-			if message != text {
-				fmt.Fprintf(conn, text+"\n")
-			} else if message == text {
-				//when exit is transmitet close Connection
-				message = strings.TrimRight(message, "\r\n")
-				if message == "exit" {
-					fmt.Println("Close Client Connection\n")
-					conn.Close()
-					fmt.Print("Last Message from Server: " + message)
-					return
-				}
-				fmt.Print("Last Message from Server: " + message + "\n")
-			}
-			break
-		}
+func main() {
+	log.SetFlags(log.Lshortfile)
+
+	conf := &tls.Config{
+		//InsecureSkipVerify: true,
 	}
+
+	conn, err := tls.Dial("tcp", "127.0.0.1:443", conf)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer conn.Close()
+
+	n, err := conn.Write([]byte("hello\n"))
+	if err != nil {
+		log.Println(n, err)
+		return
+	}
+
+	buf := make([]byte, 100)
+	n, err = conn.Read(buf)
+	if err != nil {
+		log.Println(n, err)
+		return
+	}
+
+	println(string(buf[:n]))
 }
